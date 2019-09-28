61a0fc60c8b647dee2ca809b6a22e8d4090f71b1
MeThecCoder4 <ryszard.sinius@gmail.com>
2019-09-28 19:20:23 +0200

    Basic game settings added.

--- main.js
+++ main.js
@@ -1,19 +1,46 @@
 'use strict';
 
-const numberOfRows = 40;
+let numberOfRows = 5;
 const canvas = document.getElementById('gameOfLife');
-const cellWidth = canvas.width / numberOfRows;
+let cellWidth = canvas.width / numberOfRows;
 const ctx = canvas.getContext('2d');
+const sliders = document.querySelectorAll('input');
+const sliderTitles = document.querySelectorAll('p');
+const applyButton = document.getElementById('bt-apply');
+let currentIntervalId;
+let lastGridSize = +sliders[0].value;
 ctx.font = '20px';
 
 let cells = createCells(numberOfRows);
-updateNeighbours(cells, numberOfRows);
 drawCellGrid(ctx, cells, numberOfRows, cellWidth);
 
-setInterval(() => {
-  cells = updateCells(cells, numberOfRows);
+for(let i = 0; i < sliders.length; i++) {
+  sliders[i].oninput = function() {
+    for(let i = 0; i < sliders.length; i++) {
+      const titleText = sliderTitles[i].textContent.split(':')[0];
+      sliderTitles[i].textContent = titleText + ': ' + sliders[i].value;
+    }
+  }
+}
+
+applyButton.onclick = function() {
+  if(+sliders[0].value !== lastGridSize) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    numberOfRows = +sliders[0].value;
+    cells = createCells(numberOfRows);
+    lastGridSize = numberOfRows;
+    cellWidth = canvas.width / numberOfRows;
+  }
+
+  initCells(cells, numberOfRows, +sliders[2].value);
+  updateNeighbours(cells, numberOfRows);
   drawCellGrid(ctx, cells, numberOfRows, cellWidth);
-}, 250);
+  clearInterval(currentIntervalId);
+  currentIntervalId = setInterval(() => {
+    cells = updateCells(cells, numberOfRows);
+    drawCellGrid(ctx, cells, numberOfRows, cellWidth);
+  }, +sliders[1].value);
+}
 
 // Functions
 
@@ -27,7 +54,8 @@ function drawCellGrid(ctx, cells, numberOfRows, cellWidth) {
     for(let x = 0; x < numberOfRows; x++) {
       if(cells[y][x].isAlive) {
         // Make an alive cell green
-        ctx.fillStyle = '#90fe55';
+        // ctx.fillStyle = '#90fe55';
+        ctx.fillStyle = '#2ef930';
         ctx.fillRect(x * cellWidth, y * cellWidth, cellWidth, cellWidth);
       }
       else {
@@ -45,15 +73,24 @@ function createCells(numberOfRows) {
   let cells = new Array(numberOfRows);
 
   for(let y = 0; y < numberOfRows; y++) {
-    // Allocate row memory
     cells[y] = new Array(numberOfRows);
 
     for(let x = 0; x < numberOfRows; x++) {
-      cells[y][x] = new Cell(Math.random() < 0.3 ? true : false);
+      cells[y][x] = new Cell;
     }
   }
 
   return cells;
+}
+
+function initCells(cells, numberOfRows, probability) {
+  for(let y = 0; y < numberOfRows; y++) {
+    for(let x = 0; x < numberOfRows; x++) {
+      Math.random() < probability / 100 ?
+        (cells[y][x].isAlive = true) :
+        (cells[y][x].isAlive = false);
+    }
+  }
 }
 
 function countCellNeighbours(cells, numberOfRows, x, y) {
